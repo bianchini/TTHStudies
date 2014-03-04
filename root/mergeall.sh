1b1131bb143463c213c8bfb7dd5e406fe9897081
Lorenzo <lore.bianchini@gmail.com>
2014-03-04 15:19:47 +0100

    as of Feb24

--- root/mergeall.sh
+++ root/mergeall.sh
@@ -1,10 +1,20 @@
 #! /bin/sh
 
+
+if [ -z $ROOTSYS ]; then
+ echo "ROOTSYS is not defined: source ROOT, or hadd won't work!"
+ exit
+fi
+
+
 # ANALYSIS:
 #  0 if only nominal 
 #  1 if only systematics
 #  2 if both 
 DOSYS=0
+
+# name tag for nominal analysis
+NOMINAL=all
 
 # secondary name of the trees
 NAME=New
@@ -13,82 +23,138 @@ NAME=New
 ls -ltr
 
 if [ $DOSYS == 0 ] || [ $DOSYS == 2 ] ; then
-    ./hadd.sh  SL_$1_nominal_$2_TTJetsSemiLept  $NAME
-    ./hadd.sh  SL_$1_nominal_$2_TTJetsFullLept  $NAME
-    ./hadd.sh  SL_$1_nominal_$2_TTH125          $NAME
-    ./hadd.sh  SL_$1_nominal_$2_TTZ             $NAME
-    ./hadd.sh  SL_$1_nominal_$2_DYJets10to50    $NAME
-    ./hadd.sh  SL_$1_nominal_$2_DYJets50        $NAME
-    ./hadd.sh  SL_$1_nominal_$2_TTW             $NAME
-    ./hadd.sh  SL_$1_nominal_$2_Tt              $NAME
-    ./hadd.sh  SL_$1_nominal_$2_WJets           $NAME
-    ./hadd.sh  SL_$1_nominal_$2_Ts              $NAME
-    ./hadd.sh  SL_$1_nominal_$2_ZZ              $NAME
-    ./hadd.sh  SL_$1_nominal_$2_Tbars           $NAME
-    ./hadd.sh  SL_$1_nominal_$2_WW              $NAME
-    ./hadd.sh  SL_$1_nominal_$2_WZ              $NAME
-    ./hadd.sh  SL_$1_nominal_$2_Tbart           $NAME
-    ./hadd.sh  SL_$1_nominal_$2_TbartW          $NAME
-    ./hadd.sh  SL_$1_nominal_$2_TtW             $NAME
+    ./hadd.sh  $1${NOMINAL}_$2_TTJetsSemiLept  $NAME
+    ./hadd.sh  $1${NOMINAL}_$2_TTJetsFullLept  $NAME
+    ./hadd.sh  $1${NOMINAL}_$2_TTH125          $NAME
+    ./hadd.sh  $1${NOMINAL}_$2_TTZ             $NAME
+    ./hadd.sh  $1${NOMINAL}_$2_DYJets10to50    $NAME
+    ./hadd.sh  $1${NOMINAL}_$2_DYJets50        $NAME
+    ./hadd.sh  $1${NOMINAL}_$2_TTW             $NAME
+    ./hadd.sh  $1${NOMINAL}_$2_Tt              $NAME
+    ./hadd.sh  $1${NOMINAL}_$2_WJets           $NAME
+    ./hadd.sh  $1${NOMINAL}_$2_Ts              $NAME
+    ./hadd.sh  $1${NOMINAL}_$2_ZZ              $NAME
+    ./hadd.sh  $1${NOMINAL}_$2_Tbars           $NAME
+    ./hadd.sh  $1${NOMINAL}_$2_WW              $NAME
+    ./hadd.sh  $1${NOMINAL}_$2_WZ              $NAME
+    ./hadd.sh  $1${NOMINAL}_$2_Tbart           $NAME
+    ./hadd.sh  $1${NOMINAL}_$2_TbartW          $NAME
+    ./hadd.sh  $1${NOMINAL}_$2_TtW             $NAME
 fi
 if [ $DOSYS == 1 ] || [ $DOSYS == 2 ] ; then
-    ./hadd.sh  SL_$1_csvDown_$2_TTH125          $NAME
-    ./hadd.sh  SL_$1_csvUp_$2_TTH125            $NAME
-    ./hadd.sh  SL_$1_JECDown_$2_TTH125          $NAME
-    ./hadd.sh  SL_$1_JECUp_$2_TTH125            $NAME
-    ./hadd.sh  SL_$1_csvDown_$2_TTJetsSemiLept  $NAME
-    ./hadd.sh  SL_$1_csvUp_$2_TTJetsSemiLept    $NAME
-    ./hadd.sh  SL_$1_JECDown_$2_TTJetsSemiLept  $NAME
-    ./hadd.sh  SL_$1_JECUp_$2_TTJetsSemiLept    $NAME
-    ./hadd.sh  SL_$1_csvDown_$2_TTJetsFullLept  $NAME
-    ./hadd.sh  SL_$1_csvUp_$2_TTJetsFullLept    $NAME
-    ./hadd.sh  SL_$1_JECDown_$2_TTJetsFullLept  $NAME
-    ./hadd.sh  SL_$1_JECUp_$2_TTJetsFullLept    $NAME
+    ./hadd.sh  $1_csvDown_$2_TTH125          $NAME
+    ./hadd.sh  $1_csvUp_$2_TTH125            $NAME
+    ./hadd.sh  $1_JECDown_$2_TTH125          $NAME
+    ./hadd.sh  $1_JECUp_$2_TTH125            $NAME
+    ./hadd.sh  $1_csvDown_$2_TTJetsSemiLept  $NAME
+    ./hadd.sh  $1_csvUp_$2_TTJetsSemiLept    $NAME
+    ./hadd.sh  $1_JECDown_$2_TTJetsSemiLept  $NAME
+    ./hadd.sh  $1_JECUp_$2_TTJetsSemiLept    $NAME
+    ./hadd.sh  $1_csvDown_$2_TTJetsFullLept  $NAME
+    ./hadd.sh  $1_csvUp_$2_TTJetsFullLept    $NAME
+    ./hadd.sh  $1_JECDown_$2_TTJetsFullLept  $NAME
+    ./hadd.sh  $1_JECUp_$2_TTJetsFullLept    $NAME
 fi
 
 if [ $DOSYS == 0 ] || [ $DOSYS == 2 ] ; then
 
-    if ls MEAnalysis${NAME}_SL_$1_nominal_$2_DYJets10to50.root MEAnalysis${NAME}_SL_$1_nominal_$2_DYJets50.root MEAnalysis${NAME}_SL_$1_nominal_$2_WJets.root  &> /dev/null ; then
-	hadd -f MEAnalysis${NAME}_SL_$1_nominal_$2_EWK.root  MEAnalysis${NAME}_SL_$1_nominal_$2_DYJets10to50.root MEAnalysis${NAME}_SL_$1_nominal_$2_DYJets50.root MEAnalysis${NAME}_SL_$1_nominal_$2_WJets.root
-	rm   MEAnalysis${NAME}_SL_$1_nominal_$2_DYJets10to50.root MEAnalysis${NAME}_SL_$1_nominal_$2_DYJets50.root MEAnalysis${NAME}_SL_$1_nominal_$2_WJets.root
+    if ls MEAnalysis${NAME}_$1${NOMINAL}_$2_DYJets10to50.root MEAnalysis${NAME}_$1${NOMINAL}_$2_DYJets50.root MEAnalysis${NAME}_$1${NOMINAL}_$2_WJets.root  &> /dev/null ; then
+	hadd -f MEAnalysis${NAME}_$1${NOMINAL}_$2_EWK.root  MEAnalysis${NAME}_$1${NOMINAL}_$2_DYJets10to50.root MEAnalysis${NAME}_$1${NOMINAL}_$2_DYJets50.root MEAnalysis${NAME}_$1${NOMINAL}_$2_WJets.root
+	if ls  MEAnalysis${NAME}_$1${NOMINAL}_$2_EWK.root  &> /dev/null ; then
+	    rm   MEAnalysis${NAME}_$1${NOMINAL}_$2_DYJets10to50.root MEAnalysis${NAME}_$1${NOMINAL}_$2_DYJets50.root MEAnalysis${NAME}_$1${NOMINAL}_$2_WJets.root
+	fi
     fi
 
-    if ls MEAnalysis${NAME}_SL_$1_nominal_$2_Tt.root MEAnalysis${NAME}_SL_$1_nominal_$2_Ts.root MEAnalysis${NAME}_SL_$1_nominal_$2_Tbars.root MEAnalysis${NAME}_SL_$1_nominal_$2_Tbart.root MEAnalysis${NAME}_SL_$1_nominal_$2_TbartW.root MEAnalysis${NAME}_SL_$1_nominal_$2_TtW.root   &> /dev/null ; then
-	hadd -f  MEAnalysis${NAME}_SL_$1_nominal_$2_SingleT.root MEAnalysis${NAME}_SL_$1_nominal_$2_Tt.root MEAnalysis${NAME}_SL_$1_nominal_$2_Ts.root MEAnalysis${NAME}_SL_$1_nominal_$2_Tbars.root MEAnalysis${NAME}_SL_$1_nominal_$2_Tbart.root MEAnalysis${NAME}_SL_$1_nominal_$2_TbartW.root MEAnalysis${NAME}_SL_$1_nominal_$2_TtW.root
-	rm MEAnalysis${NAME}_SL_$1_nominal_$2_Tt.root MEAnalysis${NAME}_SL_$1_nominal_$2_Ts.root MEAnalysis${NAME}_SL_$1_nominal_$2_Tbars.root MEAnalysis${NAME}_SL_$1_nominal_$2_Tbart.root MEAnalysis${NAME}_SL_$1_nominal_$2_TbartW.root MEAnalysis${NAME}_SL_$1_nominal_$2_TtW.root
+    if ls MEAnalysis${NAME}_$1${NOMINAL}_$2_Tt.root MEAnalysis${NAME}_$1${NOMINAL}_$2_Ts.root MEAnalysis${NAME}_$1${NOMINAL}_$2_Tbars.root MEAnalysis${NAME}_$1${NOMINAL}_$2_Tbart.root MEAnalysis${NAME}_$1${NOMINAL}_$2_TbartW.root MEAnalysis${NAME}_$1${NOMINAL}_$2_TtW.root   &> /dev/null ; then
+	hadd -f  MEAnalysis${NAME}_$1${NOMINAL}_$2_SingleT.root MEAnalysis${NAME}_$1${NOMINAL}_$2_Tt.root MEAnalysis${NAME}_$1${NOMINAL}_$2_Ts.root MEAnalysis${NAME}_$1${NOMINAL}_$2_Tbars.root MEAnalysis${NAME}_$1${NOMINAL}_$2_Tbart.root MEAnalysis${NAME}_$1${NOMINAL}_$2_TbartW.root MEAnalysis${NAME}_$1${NOMINAL}_$2_TtW.root
+	if  ls MEAnalysis${NAME}_$1${NOMINAL}_$2_SingleT.root  &> /dev/null ; then
+	    rm MEAnalysis${NAME}_$1${NOMINAL}_$2_Tt.root MEAnalysis${NAME}_$1${NOMINAL}_$2_Ts.root MEAnalysis${NAME}_$1${NOMINAL}_$2_Tbars.root MEAnalysis${NAME}_$1${NOMINAL}_$2_Tbart.root MEAnalysis${NAME}_$1${NOMINAL}_$2_TbartW.root MEAnalysis${NAME}_$1${NOMINAL}_$2_TtW.root
+	fi
     fi
 
-    if ls MEAnalysis${NAME}_SL_$1_nominal_$2_ZZ.root MEAnalysis${NAME}_SL_$1_nominal_$2_WW.root MEAnalysis${NAME}_SL_$1_nominal_$2_WZ.root &> /dev/null ; then
-	hadd -f  MEAnalysis${NAME}_SL_$1_nominal_$2_DiBoson.root MEAnalysis${NAME}_SL_$1_nominal_$2_ZZ.root MEAnalysis${NAME}_SL_$1_nominal_$2_WW.root MEAnalysis${NAME}_SL_$1_nominal_$2_WZ.root
-	rm  MEAnalysis${NAME}_SL_$1_nominal_$2_ZZ.root MEAnalysis${NAME}_SL_$1_nominal_$2_WW.root MEAnalysis${NAME}_SL_$1_nominal_$2_WZ.root
+    if ls MEAnalysis${NAME}_$1${NOMINAL}_$2_ZZ.root MEAnalysis${NAME}_$1${NOMINAL}_$2_WW.root MEAnalysis${NAME}_$1${NOMINAL}_$2_WZ.root &> /dev/null ; then
+	hadd -f  MEAnalysis${NAME}_$1${NOMINAL}_$2_DiBoson.root MEAnalysis${NAME}_$1${NOMINAL}_$2_ZZ.root MEAnalysis${NAME}_$1${NOMINAL}_$2_WW.root MEAnalysis${NAME}_$1${NOMINAL}_$2_WZ.root
+	if ls  MEAnalysis${NAME}_$1${NOMINAL}_$2_DiBoson.root  &> /dev/null ; then
+	    rm  MEAnalysis${NAME}_$1${NOMINAL}_$2_ZZ.root MEAnalysis${NAME}_$1${NOMINAL}_$2_WW.root MEAnalysis${NAME}_$1${NOMINAL}_$2_WZ.root
+	fi
     fi
 
-    if ls MEAnalysis${NAME}_SL_$1_nominal_$2_TTZ.root MEAnalysis${NAME}_SL_$1_nominal_$2_TTW.root &> /dev/null ; then
-	hadd -f  MEAnalysis${NAME}_SL_$1_nominal_$2_TTV.root MEAnalysis${NAME}_SL_$1_nominal_$2_TTZ.root MEAnalysis${NAME}_SL_$1_nominal_$2_TTW.root
-	rm MEAnalysis${NAME}_SL_$1_nominal_$2_TTZ.root MEAnalysis${NAME}_SL_$1_nominal_$2_TTW.root
+    if ls MEAnalysis${NAME}_$1${NOMINAL}_$2_TTZ.root MEAnalysis${NAME}_$1${NOMINAL}_$2_TTW.root &> /dev/null ; then
+	hadd -f  MEAnalysis${NAME}_$1${NOMINAL}_$2_TTV.root MEAnalysis${NAME}_$1${NOMINAL}_$2_TTZ.root MEAnalysis${NAME}_$1${NOMINAL}_$2_TTW.root
+	if ls  MEAnalysis${NAME}_$1${NOMINAL}_$2_TTV.root &> /dev/null ; then
+	    rm MEAnalysis${NAME}_$1${NOMINAL}_$2_TTZ.root MEAnalysis${NAME}_$1${NOMINAL}_$2_TTW.root
+	fi
     fi
 
-    if ls MEAnalysis${NAME}_SL_$1_nominal_$2_TTJetsFullLept.root MEAnalysis${NAME}_SL_$1_nominal_$2_TTJetsSemiLept.root  &> /dev/null ; then
-	hadd -f MEAnalysis${NAME}_SL_$1_nominal_$2_TTJets.root    MEAnalysis${NAME}_SL_$1_nominal_$2_TTJetsFullLept.root MEAnalysis${NAME}_SL_$1_nominal_$2_TTJetsSemiLept.root 
-	rm MEAnalysis${NAME}_SL_$1_nominal_$2_TTJetsFullLept.root MEAnalysis${NAME}_SL_$1_nominal_$2_TTJetsSemiLept.root
+    if ls MEAnalysis${NAME}_$1${NOMINAL}_$2_TTJetsFullLept.root MEAnalysis${NAME}_$1${NOMINAL}_$2_TTJetsSemiLept.root  &> /dev/null ; then
+	hadd -f MEAnalysis${NAME}_$1${NOMINAL}_$2_TTJets.root    MEAnalysis${NAME}_$1${NOMINAL}_$2_TTJetsFullLept.root MEAnalysis${NAME}_$1${NOMINAL}_$2_TTJetsSemiLept.root 
+	if ls MEAnalysis${NAME}_$1${NOMINAL}_$2_TTJets.root  &> /dev/null ; then
+	    rm MEAnalysis${NAME}_$1${NOMINAL}_$2_TTJetsFullLept.root MEAnalysis${NAME}_$1${NOMINAL}_$2_TTJetsSemiLept.root
+	fi
     fi
 fi
 
 if [ $DOSYS == 1 ] || [ $DOSYS == 2 ] ; then
-    if ls  MEAnalysis${NAME}_SL_$1_csvDown_$2_TTJetsFullLept.root MEAnalysis${NAME}_SL_$1_csvDown_$2_TTJetsSemiLept.root  &> /dev/null ; then 
-	hadd -f MEAnalysis${NAME}_SL_$1_csvDown_$2_TTJets.root    MEAnalysis${NAME}_SL_$1_csvDown_$2_TTJetsFullLept.root MEAnalysis${NAME}_SL_$1_csvDown_$2_TTJetsSemiLept.root 
-	rm MEAnalysis${NAME}_SL_$1_csvDown_$2_TTJetsFullLept.root MEAnalysis${NAME}_SL_$1_csvDown_$2_TTJetsSemiLept.root
+    if ls  MEAnalysis${NAME}_$1_csvDown_$2_TTJetsFullLept.root MEAnalysis${NAME}_$1_csvDown_$2_TTJetsSemiLept.root  &> /dev/null ; then 
+	hadd -f MEAnalysis${NAME}_$1_csvDown_$2_TTJets.root    MEAnalysis${NAME}_$1_csvDown_$2_TTJetsFullLept.root MEAnalysis${NAME}_$1_csvDown_$2_TTJetsSemiLept.root 
+	if ls MEAnalysis${NAME}_$1_csvDown_$2_TTJets.root  &> /dev/null ; then
+	    rm MEAnalysis${NAME}_$1_csvDown_$2_TTJetsFullLept.root MEAnalysis${NAME}_$1_csvDown_$2_TTJetsSemiLept.root
+	fi
     fi
-    if ls MEAnalysis${NAME}_SL_$1_csvUp_$2_TTJetsFullLept.root   MEAnalysis${NAME}_SL_$1_csvUp_$2_TTJetsSemiLept.root  &> /dev/null ; then 
-	hadd -f MEAnalysis${NAME}_SL_$1_csvUp_$2_TTJets.root      MEAnalysis${NAME}_SL_$1_csvUp_$2_TTJetsFullLept.root   MEAnalysis${NAME}_SL_$1_csvUp_$2_TTJetsSemiLept.root 
-	rm MEAnalysis${NAME}_SL_$1_csvUp_$2_TTJetsFullLept.root   MEAnalysis${NAME}_SL_$1_csvUp_$2_TTJetsSemiLept.root 
+    if ls MEAnalysis${NAME}_$1_csvUp_$2_TTJetsFullLept.root   MEAnalysis${NAME}_$1_csvUp_$2_TTJetsSemiLept.root  &> /dev/null ; then 
+	hadd -f MEAnalysis${NAME}_$1_csvUp_$2_TTJets.root      MEAnalysis${NAME}_$1_csvUp_$2_TTJetsFullLept.root   MEAnalysis${NAME}_$1_csvUp_$2_TTJetsSemiLept.root 
+	if ls MEAnalysis${NAME}_$1_csvUp_$2_TTJets.root  &> /dev/null ; then
+	    rm MEAnalysis${NAME}_$1_csvUp_$2_TTJetsFullLept.root   MEAnalysis${NAME}_$1_csvUp_$2_TTJetsSemiLept.root 
+	fi
     fi
-    if ls MEAnalysis${NAME}_SL_$1_JECDown_$2_TTJetsFullLept.root MEAnalysis${NAME}_SL_$1_JECDown_$2_TTJetsSemiLept.root   &> /dev/null ; then 
-	hadd -f MEAnalysis${NAME}_SL_$1_JECDown_$2_TTJets.root    MEAnalysis${NAME}_SL_$1_JECDown_$2_TTJetsFullLept.root MEAnalysis${NAME}_SL_$1_JECDown_$2_TTJetsSemiLept.root 
-	rm MEAnalysis${NAME}_SL_$1_JECDown_$2_TTJetsFullLept.root MEAnalysis${NAME}_SL_$1_JECDown_$2_TTJetsSemiLept.root    
+    if ls MEAnalysis${NAME}_$1_JECDown_$2_TTJetsFullLept.root MEAnalysis${NAME}_$1_JECDown_$2_TTJetsSemiLept.root   &> /dev/null ; then 
+	hadd -f MEAnalysis${NAME}_$1_JECDown_$2_TTJets.root    MEAnalysis${NAME}_$1_JECDown_$2_TTJetsFullLept.root MEAnalysis${NAME}_$1_JECDown_$2_TTJetsSemiLept.root 
+	if ls MEAnalysis${NAME}_$1_JECDown_$2_TTJets.root   &> /dev/null ; then
+	    rm MEAnalysis${NAME}_$1_JECDown_$2_TTJetsFullLept.root MEAnalysis${NAME}_$1_JECDown_$2_TTJetsSemiLept.root    
+	fi
     fi
-    if ls MEAnalysis${NAME}_SL_$1_JECUp_$2_TTJetsFullLept.root   MEAnalysis${NAME}_SL_$1_JECUp_$2_TTJetsSemiLept.root  &> /dev/null ; then 
-	hadd -f MEAnalysis${NAME}_SL_$1_JECUp_$2_TTJets.root      MEAnalysis${NAME}_SL_$1_JECUp_$2_TTJetsFullLept.root   MEAnalysis${NAME}_SL_$1_JECUp_$2_TTJetsSemiLept.root 
-	rm MEAnalysis${NAME}_SL_$1_JECUp_$2_TTJetsFullLept.root   MEAnalysis${NAME}_SL_$1_JECUp_$2_TTJetsSemiLept.root 
+    if ls MEAnalysis${NAME}_$1_JECUp_$2_TTJetsFullLept.root   MEAnalysis${NAME}_$1_JECUp_$2_TTJetsSemiLept.root  &> /dev/null ; then 
+	hadd -f MEAnalysis${NAME}_$1_JECUp_$2_TTJets.root      MEAnalysis${NAME}_$1_JECUp_$2_TTJetsFullLept.root   MEAnalysis${NAME}_$1_JECUp_$2_TTJetsSemiLept.root 
+	if ls MEAnalysis${NAME}_$1_JECUp_$2_TTJets.root    &> /dev/null ; then
+	    rm MEAnalysis${NAME}_$1_JECUp_$2_TTJetsFullLept.root   MEAnalysis${NAME}_$1_JECUp_$2_TTJetsSemiLept.root 
+	fi
     fi
+fi
+
+
+
+if ls ./MEAnalysis${NAME}_$1${NOMINAL}_$2_*SingleMu*_p* &> /dev/null ; then
+    ls MEAnalysis${NAME}_$1${NOMINAL}_$2_*SingleMu*_p*.root
+    hadd -f MEAnalysis${NAME}_$1${NOMINAL}_$2_Run2012_SingleMu.root MEAnalysis${NAME}_$1${NOMINAL}_$2_*SingleMu*_p*.root 
+    #echo "Hadd"
+    if ls  MEAnalysis${NAME}_$1${NOMINAL}_$2_Run2012_SingleMu.root  &> /dev/null ; then
+	#echo "Remove"
+	rm MEAnalysis${NAME}_$1${NOMINAL}_$2_*SingleMu*_p*.root
+    fi
+else
+    echo "MEAnalysis${NAME}_*SingleMu* not found"
+fi
+
+if ls ./MEAnalysis${NAME}_$1${NOMINAL}_$2_*SingleElectron*_p* &> /dev/null ; then
+    ls MEAnalysis${NAME}_$1${NOMINAL}_$2_*SingleElectron*_p*.root
+    hadd -f MEAnalysis${NAME}_$1${NOMINAL}_$2_Run2012_SingleElectron.root MEAnalysis${NAME}_$1${NOMINAL}_$2_*SingleElectron*_p*.root 
+    #echo "Hadd"
+    if ls  MEAnalysis${NAME}_$1${NOMINAL}_$2_Run2012_SingleElectron.root  &> /dev/null ; then
+	#echo "Remove"
+	rm MEAnalysis${NAME}_$1${NOMINAL}_$2_*SingleElectron*_p*.root
+    fi
+else
+    echo "MEAnalysis${NAME}_*SingleElectron* not found"
+fi
+
+if ls ./MEAnalysis${NAME}_$1${NOMINAL}_$2_*DoubleElectron*_p* &> /dev/null ; then
+    ls MEAnalysis${NAME}_$1${NOMINAL}_$2_*DoubleElectron*_p*.root
+    hadd -f MEAnalysis${NAME}_$1${NOMINAL}_$2_Run2012_DoubleElectron.root MEAnalysis${NAME}_$1${NOMINAL}_$2_*DoubleElectron*_p*.root 
+    #echo "Hadd"
+    if ls  MEAnalysis${NAME}_$1${NOMINAL}_$2_Run2012_DoubleElectron.root  &> /dev/null ; then
+	#echo "Remove"
+	rm MEAnalysis${NAME}_$1${NOMINAL}_$2_*DoubleElectron*_p*.root
+    fi
+else
+    echo "MEAnalysis${NAME}_*DoubleElectron* not found"
 fi
--- root/store.sh
+++ root/store.sh
@@ -1,14 +1,14 @@
 #! /bin/sh
 
 
-VERSION=""
+VERSION="all_rec_std"
 
-data=$(ls *TEST*${VERSION}*)
+data=$(ls *${VERSION}*)
 outdir="srm://t3se01.psi.ch:8443/srm/managerv2?SFN=/pnfs/psi.ch/cms/trivcat/store/user/bianchi/Trees/MEM/"
 
 for name in ${data}
 do
   echo "Copy: " $name 
-  #echo " -->  " ${outdir}"/"$1"/"${name}
+  echo " -->  " ${outdir}"/"$1"/"${name}
   srmcp -2  file:///$name  ${outdir}"/"$1"/"${name}
 done
