bd26c1d41ec464934f93d3f468b6d90914d17538
Lorenzo <lore.bianchini@gmail.com>
2014-03-11 09:12:03 +0100

    new permutations

--- bin/submitAllMEAnalysisNew_all.py
+++ bin/submitAllMEAnalysisNew_all.py
@@ -34,7 +34,7 @@ useTF       = 1
 usePDF      = 1
 
 # to print intermediate steps
-printout    = 0
+printout    = 1
 
 # speed up the job not doing VEGAS integration
 speedup     = 0
@@ -45,7 +45,7 @@ btag_prob_cut_5jets = 0.98225 # <--- 0.992
 btag_prob_cut_4jets = 0.95295 # <--- 0.85 <--- 0.95295 <--- 0.992
 
 # regression
-useRegression = 1
+useRegression = 0
 
 # use gen-jets or reco-jets ???
 doGenLevelAnalysis = 0
@@ -59,10 +59,16 @@ csv_WP_M = 0.679
 csv_WP_T = 0.898
 
 # select by btag_LR
-selectByBTagShape = 1
+selectByBTagShape  = 1
 
 # recover the <4 btag bin
-recoverTopBTagBin = 1
+recoverTopBTagBin  = 1
+
+# test SLw1j hypothesis on type3 events
+testSLw1jType3     = 1
+
+# test hypo SLw1j on up to nMaxJetsSLw1jType3 untagged jets
+nMaxJetsSLw1jType3 = 4
 
 # an extra name for the output files
 extraoutname = ""
@@ -126,41 +132,30 @@ def submitMEAnalysisNew_all(script,
     process.fwliteInput.integralOption1  = cms.untracked.int32( integralOption1 )
     process.fwliteInput.integralOption2  = cms.untracked.int32( integralOption2 )
 
-    process.fwliteInput.selectByBTagShape = cms.untracked.int32(selectByBTagShape)
-    process.fwliteInput.recoverTopBTagBin = cms.untracked.int32(recoverTopBTagBin)
-    
-    process.fwliteInput.doType6ByBTagShape = cms.untracked.int32(    selectByBTagShape)
+    process.fwliteInput.selectByBTagShape  = cms.untracked.int32(selectByBTagShape)
+    process.fwliteInput.recoverTopBTagBin  = cms.untracked.int32(recoverTopBTagBin)
+
+    process.fwliteInput.testSLw1jType3     = cms.untracked.int32(testSLw1jType3)
+    process.fwliteInput.nMaxJetsSLw1jType3 = cms.untracked.int32(nMaxJetsSLw1jType3)
+
+    process.fwliteInput.doType0            = cms.untracked.int32(not selectByBTagShape)
+    process.fwliteInput.doType1            = cms.untracked.int32(not selectByBTagShape)
+    process.fwliteInput.doType2            = cms.untracked.int32(not selectByBTagShape)
+    process.fwliteInput.doType3            = cms.untracked.int32(not selectByBTagShape)
     process.fwliteInput.doType6            = cms.untracked.int32(not selectByBTagShape)
     process.fwliteInput.doType7            = cms.untracked.int32(0)
 
+    #process.fwliteInput.doType0ByBTagShape = cms.untracked.int32(    selectByBTagShape)
+    #process.fwliteInput.doType1ByBTagShape = cms.untracked.int32(    selectByBTagShape)
+    #process.fwliteInput.doType2ByBTagShape = cms.untracked.int32(    selectByBTagShape)        
+    #process.fwliteInput.doType3ByBTagShape = cms.untracked.int32(    selectByBTagShape)
+    #process.fwliteInput.doType6ByBTagShape = cms.untracked.int32(    selectByBTagShape)
     process.fwliteInput.doType0ByBTagShape = cms.untracked.int32(    selectByBTagShape)
-
-    if len(massesH) == 1 and len(massesT) == 1:
-        process.fwliteInput.doType1ByBTagShape = cms.untracked.int32(    selectByBTagShape)
-    else:
-        process.fwliteInput.doType1ByBTagShape = cms.untracked.int32(0)        
-
-    process.fwliteInput.doType2ByBTagShape = cms.untracked.int32(    selectByBTagShape)
-        
-    if len(massesH) == 1 and len(massesT) == 1:
-        process.fwliteInput.doType3ByBTagShape = cms.untracked.int32(    selectByBTagShape)
-    else:
-        process.fwliteInput.doType3ByBTagShape = cms.untracked.int32(0)
-
-    process.fwliteInput.doType0            = cms.untracked.int32(not selectByBTagShape)
-
-    if len(massesH) == 1 and len(massesT) == 1:
-        process.fwliteInput.doType1            = cms.untracked.int32(not selectByBTagShape)
-    else:
-        process.fwliteInput.doType1         = cms.untracked.int32(0)
-
-    process.fwliteInput.doType2             = cms.untracked.int32(not selectByBTagShape)
-
-    if len(massesH) == 1 and len(massesT) == 1:
-        process.fwliteInput.doType3         = cms.untracked.int32(not selectByBTagShape)
-    else:
-        process.fwliteInput.doType3         = cms.untracked.int32(0)
-    
+    process.fwliteInput.doType1ByBTagShape = cms.untracked.int32(    selectByBTagShape)
+    process.fwliteInput.doType2ByBTagShape = cms.untracked.int32(    0)        
+    process.fwliteInput.doType3ByBTagShape = cms.untracked.int32(    selectByBTagShape)
+    process.fwliteInput.doType6ByBTagShape = cms.untracked.int32(    0)
+  
     process.fwliteInput.btag_prob_cut_6jets = cms.untracked.double(btag_prob_cut_6jets)
     process.fwliteInput.btag_prob_cut_5jets = cms.untracked.double(btag_prob_cut_5jets)
     process.fwliteInput.btag_prob_cut_4jets = cms.untracked.double(btag_prob_cut_4jets)
@@ -253,7 +248,7 @@ def submitFullMEAnalysisNew_all( analysis ):
 
     for i in range(num_of_jobs):
         counter = counter + 1
-        #submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, i*evs_per_job+1, (i+1)*evs_per_job )
+        submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, i*evs_per_job+1, (i+1)*evs_per_job )
 
     #return
 
@@ -271,7 +266,7 @@ def submitFullMEAnalysisNew_all( analysis ):
 
     for i in range(num_of_jobs):
         counter = counter + 1
-        #submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample,  i*evs_per_job+1, (i+1)*evs_per_job )
+        submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample,  i*evs_per_job+1, (i+1)*evs_per_job )
 
  
     if re.search("all",   analysis )==None and re.search("nominal",   analysis )==None:
@@ -286,21 +281,21 @@ def submitFullMEAnalysisNew_all( analysis ):
     counter = 0
     for i in range(1):
         counter = counter + 1
-        #submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, 0, -1 )
+        submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, 0, -1 )
 
     # DYJets50
     sample  = 'DYJets50'
     counter = 0
     for i in range(1):
         counter = counter + 1
-        #submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, 0, -1 )
+        submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, 0, -1 )
 
     # WJets 
     sample  = 'WJets'
     counter = 0
     for i in range(1):
         counter = counter + 1
-        #submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, 0, -1 )
+        submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, 0, -1 )
 
     ###################################################### Single-top
     ######################################################
@@ -310,42 +305,42 @@ def submitFullMEAnalysisNew_all( analysis ):
     counter = 0
     for i in range(1):
         counter = counter + 1
-        #submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, 0, -1 )
+        submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, 0, -1 )
         
    # Tt
     sample  = 'Tt'
     counter = 0
     for i in range(1):
         counter = counter + 1
-        #submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, 0, -1 )
+        submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, 0, -1 )
 
    # Ts
     sample  = 'Ts'
     counter = 0
     for i in range(1):
         counter = counter + 1
-        #submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, 0, -1 )
+        submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, 0, -1 )
 
     # TbartW
     sample  = 'TbartW'
     counter = 0
     for i in range(1):
         counter = counter + 1
-        #submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, 0, -1 )
+        submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, 0, -1 )
 
     # Tbart
     sample  = 'Tbart'
     counter = 0
     for i in range(1):
         counter = counter + 1
-        #submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, 0, -1 )
+        submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, 0, -1 )
 
     # Tbars
     sample  = 'Tbars'
     counter = 0
     for i in range(1):
         counter = counter + 1
-        #submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, 0, -1 )
+        submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, 0, -1 )
 
 
     ###################################################### Di-boson
@@ -357,21 +352,21 @@ def submitFullMEAnalysisNew_all( analysis ):
     counter = 0
     for i in range(1):
         counter = counter + 1
-        #submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, 0, -1 )
+        submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, 0, -1 )
 
    # WZ
     sample  = 'WZ'
     counter = 0
     for i in range(1):
         counter = counter + 1
-        #submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, 0, -1 )
+        submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, 0, -1 )
 
    # ZZ
     sample  = 'ZZ'
     counter = 0
     for i in range(1):
         counter = counter + 1
-        #submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, 0, -1 )
+        submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, 0, -1 )
 
 
     ###################################################### TTZ
@@ -388,14 +383,14 @@ def submitFullMEAnalysisNew_all( analysis ):
 
     for i in range(num_of_jobs):
         counter = counter + 1
-        #submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, i*evs_per_job+1, (i+1)*evs_per_job )
+        submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, i*evs_per_job+1, (i+1)*evs_per_job )
 
     # TTW
     sample  = 'TTW'
     counter = 0
     for i in range(1):
         counter = counter + 1
-        #submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, 0, -1 )
+        submitMEAnalysisNew_all(analysis+'_'+sample+'_p'+str(counter), sample, 0, -1 )
 
 
     ###################################################### Data
@@ -493,7 +488,7 @@ def submitFullMEAnalysisNew_all( analysis ):
 ###########################################
 
 
-analyses = ['all']
+analyses = ['all_testNewType3']
 
 for analysis in analyses:
     if doGenLevelAnalysis:
