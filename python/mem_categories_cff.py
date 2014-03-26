7970df47dc0134b7d3c5f5dbb717209ed5d38406
Lorenzo <lore.bianchini@gmail.com>
2014-03-26 18:18:19 +0100

    new cats

--- python/mem_categories_cff.py
+++ python/mem_categories_cff.py
@@ -32,7 +32,7 @@ cat = cms.PSet(
 
 cat1_bj = cat.clone(
     extraname = cms.string("_bj"),
-    cut       = cms.string("((type==0 && btag_LR>=0.975) || (type==3 && flag_type3>0  && btag_LR>=0.975))"),
+    cut       = cms.string("( type==0 || (type==3 && flag_type3>0)) && btag_LR>=0.975"), #0.975
     category  = cms.string("cat1"),
     doMEM     = cms.int32(3),
     factbb    = cms.double(0.15)
@@ -40,7 +40,7 @@ cat1_bj = cat.clone(
 
 cat2_bj = cat.clone(
     extraname = cms.string("_bj"),
-    cut       = cms.string("((type==1 && btag_LR>=0.975) || (type==3 && flag_type3<=0 && btag_LR>=0.975))"),
+    cut       = cms.string("( type==1 || (type==3 && flag_type3<=0) ) && btag_LR>=0.975"),#0.975
     category  = cms.string("cat2"),
     doMEM     = cms.int32(3),
     factbb    = cms.double(0.20)
@@ -48,7 +48,7 @@ cat2_bj = cat.clone(
 
 cat3_bj = cat.clone(
     extraname = cms.string("_bj"),
-    cut       = cms.string("type==2 && flag_type2<=999 && btag_LR>=0.990"),
+    cut       = cms.string("type==2 && flag_type2<=999 && btag_LR>=0.990"), #0.990
     category  = cms.string("cat3"),
     doMEM     = cms.int32(3),
     factbb    = cms.double(0.50)
@@ -56,7 +56,7 @@ cat3_bj = cat.clone(
 
 cat6_bj = cat.clone(
     extraname = cms.string("_bj"),
-    cut       = cms.string("type==6 && btag_LR>=0.953"),
+    cut       = cms.string("type==6 && btag_LR>=0.953"),#0.980
     category  = cms.string("cat6"),
     factbb    = cms.double(0.50),
     doMEM     = cms.int32(3),
@@ -91,7 +91,7 @@ cat3_sb_nb =  cat3_bj.clone(
 cat6_sb_nb =  cat6_bj.clone(
     extraname = cms.string("_sb_nb"),
     doMEM     = cms.int32(-2),
-    fact1     = cms.double(0.2),
+    fact1     = cms.double(0.6),
     splitFirstBin = cms.int32(1),    
     )
 
@@ -118,3 +118,76 @@ cat6_sb =  cat6_sb_nb.clone(
     doMEM     = cms.int32(2),
     )
 
+
+
+
+#################### ttbb vs ttH separation (no bias) !!! new !!!
+
+cat1_sb_H =  cat1_bj.clone(
+    cut           = cms.string("( type==0 || (type==3 && flag_type3>0)) && btag_LR>=0.995"),
+    category      = cms.string("cat1_H"),
+    extraname     = cms.string("_sb"),
+    doMEM         = cms.int32(2),
+    fact1         = cms.double(1.2),
+    splitFirstBin = cms.int32(1),
+    )
+
+
+cat1_sb_L =  cat1_bj.clone(
+    cut           = cms.string("( type==0 || (type==3 && flag_type3>0)) && btag_LR<0.995"),
+    category      = cms.string("cat1_L"),
+    extraname     = cms.string("_sb"),
+    doMEM         = cms.int32(2),
+    fact1         = cms.double(1.2),
+    splitFirstBin = cms.int32(0),
+    )
+
+
+cat2_sb_H =  cat2_bj.clone(
+    cut       = cms.string("( type==1 || (type==3 && flag_type3<=0) ) && btag_LR>=0.9925"),
+    category  = cms.string("cat2_H"), 
+    extraname = cms.string("_sb"),
+    doMEM     = cms.int32(2),
+    fact1     = cms.double(0.6),
+    splitFirstBin = cms.int32(1),
+    )
+
+cat2_sb_L =  cat2_bj.clone(
+    cut       = cms.string("( type==1 || (type==3 && flag_type3<=0) ) && btag_LR<0.9925"),
+    category  = cms.string("cat2_L"), 
+    extraname = cms.string("_sb"),
+    doMEM     = cms.int32(2),
+    fact1     = cms.double(1.8),
+    splitFirstBin = cms.int32(0),
+    )
+
+
+cat3_sb_H =  cat3_bj.clone(
+    cut       = cms.string("type==2 && flag_type2<=999 && btag_LR>=0.995"), 
+    category  = cms.string("cat3_H"), 
+    extraname = cms.string("_sb"),
+    doMEM     = cms.int32(2),
+    fact1     = cms.double(0.6),
+    splitFirstBin = cms.int32(1),   
+    )
+
+cat3_sb_L =  cat3_bj.clone(
+    cut       = cms.string("type==2 && flag_type2<=999 && btag_LR<0.995"), 
+    category  = cms.string("cat3_L"), 
+    extraname = cms.string("_sb"),
+    doMEM     = cms.int32(2),
+    fact1     = cms.double(2.0),
+    splitFirstBin = cms.int32(0),   
+    )
+
+cat6_sb_H =  cat6_bj.clone(
+    cut       = cms.string("type==6 && btag_LR>=0.953"),
+    category  = cms.string("cat6_H"), 
+    extraname = cms.string("_sb"),
+    doMEM     = cms.int32(2),
+    fact1     = cms.double(0.6),
+    splitFirstBin = cms.int32(1),
+    nBins     = cms.int32(6),
+    )
+
+cat6_sb_L = cat6_sb_H.clone()
