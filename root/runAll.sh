7fdf949458da0b81f05aba6901267cd99c8d1c5e
Lorenzo <lore.bianchini@gmail.com>
2014-04-08 10:51:34 +0200

    script for limits

--- root/runAll.sh
+++ root/runAll.sh
@@ -3,20 +3,34 @@
 OPTION='Asymptotic -t -1'
 #OPTION='MaxLikelihoodFit '
 
-combineCards.py Name1=MEM_cat1_$1.txt Name2=MEM_cat2_$1.txt Name3=MEM_cat3_$1.txt Name4=MEM_cat4_$1.txt Name5=MEM_cat5_$1.txt> MEM_SL_$1.txt
+NEW=1
 
-combineCards.py Name1=MEM_cat6_$1.txt > MEM_DL_$1.txt 
+if [ $NEW == 1 ] ; then
+    combineCards.py Name1=MEM_cat1_H_$1.txt Name2=MEM_cat1_L_$1.txt Name3=MEM_cat2_H_$1.txt Name4=MEM_cat2_L_$1.txt  Name5=MEM_cat3_H_$1.txt Name6=MEM_cat3_L_$1.txt > MEM_SL_$1.txt
+    combineCards.py Name1=MEM_cat6_H_$1.txt > MEM_DL_$1.txt
+    combineCards.py Name1=MEM_SL_$1.txt Name2=MEM_DL_$1.txt > MEM_COMB_$1.txt
+    combine -M ${OPTION} -n MEM_COMB_$1  -d MEM_COMB_$1.txt 
+    combine -M ${OPTION} -n MEM_SL_$1    -d MEM_SL_$1.txt 
+    combine -M ${OPTION} -n MEM_DL_$1    -d MEM_DL_$1.txt 
+    combine -M ${OPTION} -n MEM_cat1_H_$1  -d MEM_cat1_H_$1.txt 
+    combine -M ${OPTION} -n MEM_cat1_L_$1  -d MEM_cat1_L_$1.txt 
+    combine -M ${OPTION} -n MEM_cat2_H_$1  -d MEM_cat2_H_$1.txt 
+    combine -M ${OPTION} -n MEM_cat2_L_$1  -d MEM_cat2_L_$1.txt 
+    combine -M ${OPTION} -n MEM_cat3_H_$1  -d MEM_cat3_H_$1.txt 
+    combine -M ${OPTION} -n MEM_cat3_L_$1  -d MEM_cat3_L_$1.txt 
+    combine -M ${OPTION} -n MEM_cat6_H_$1  -d MEM_cat6_H_$1.txt 
+    combine -M ${OPTION} -n MEM_cat6_L_$1  -d MEM_cat6_H_$1.txt 
+fi
 
-combineCards.py Name1=MEM_SL_$1.txt Name2=MEM_DL_$1.txt > MEM_COMB_$1.txt
-
-
-combine -M ${OPTION} -n MEM_COMB_$1  -d MEM_COMB_$1.txt 
-combine -M ${OPTION} -n MEM_SL_$1    -d MEM_SL_$1.txt 
-combine -M ${OPTION} -n MEM_DL_$1    -d MEM_DL_$1.txt 
-
-combine -M ${OPTION} -n MEM_cat1_$1  -d MEM_cat1_$1.txt 
-combine -M ${OPTION} -n MEM_cat2_$1  -d MEM_cat2_$1.txt 
-combine -M ${OPTION} -n MEM_cat3_$1  -d MEM_cat3_$1.txt 
-combine -M ${OPTION} -n MEM_cat4_$1  -d MEM_cat4_$1.txt 
-combine -M ${OPTION} -n MEM_cat5_$1  -d MEM_cat5_$1.txt 
-combine -M ${OPTION} -n MEM_cat6_$1  -d MEM_cat6_$1.txt 
+if [ $NEW == 0 ] ; then
+    combineCards.py Name1=MEM_cat1_$1.txt Name2=MEM_cat2_$1.txt Name3=MEM_cat3_$1.txt> MEM_SL_$1.txt
+    combineCards.py Name1=MEM_cat6_$1.txt > MEM_DL_$1.txt 
+    combineCards.py Name1=MEM_SL_$1.txt Name2=MEM_DL_$1.txt > MEM_COMB_$1.txt
+    combine -M ${OPTION} -n MEM_COMB_$1  -d MEM_COMB_$1.txt 
+    combine -M ${OPTION} -n MEM_SL_$1    -d MEM_SL_$1.txt 
+    combine -M ${OPTION} -n MEM_DL_$1    -d MEM_DL_$1.txt 
+    combine -M ${OPTION} -n MEM_cat1_$1  -d MEM_cat1_$1.txt 
+    combine -M ${OPTION} -n MEM_cat2_$1  -d MEM_cat2_$1.txt 
+    combine -M ${OPTION} -n MEM_cat3_$1  -d MEM_cat3_$1.txt 
+    combine -M ${OPTION} -n MEM_cat6_$1  -d MEM_cat6_$1.txt 
+fi
--- root/runAllOptmize.sh
+++ root/runAllOptmize.sh
@@ -1,9 +1,24 @@
 #! /bin/sh
 
 
-max=6
-for i in `seq 0 $max`
-  do
-  echo "Doing $1_$2_$3_$i"
-  combine -M Asymptotic -t -1 -n $1_$2_$3_$i  -d $1_$2_$3_$i.txt 
-done
+OPTIMIZE1=1
+OPTIMIZE2=0
+
+if [ $OPTIMIZE1==1 ] ; then
+    max=8
+    for i in `seq 0 $max`
+      do
+      echo "Doing $1_$2_$3_$i"
+      combineCards.py Name1=$1_$2_$3-${i}_0.txt  Name2=$1_$2_$3-${i}_1.txt > $1_$2_$3-${i}.txt
+      combine -M Asymptotic -t -1 -n $1_$2_$3_$i  -d $1_$2_$3-$i.txt 
+    done
+fi
+
+if [ $OPTIMIZE2==1 ] ; then
+    max=8
+    for i in `seq 0 $max`
+      do
+      echo "Doing $1_$2_$3_$i"
+      combine -M Asymptotic -t -1 -n $1_$2_$3_$i  -d $1_$2_$3_$i.txt 
+    done
+fi
