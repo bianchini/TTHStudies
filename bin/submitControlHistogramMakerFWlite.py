dfcbad42e384e711ba3eb83865d0c03944a35ed8
Liis Rebane <polaarrebane@gmail.com>
2014-04-14 18:50:10 +0300

    update control histogram submission

--- bin/submitControlHistogramMakerFWlite.py
+++ bin/submitControlHistogramMakerFWlite.py
@@ -1,20 +1,79 @@
 import FWCore.ParameterSet.Config as cms
 from submitDataCardMakerFWlite import submitDataCardMakerFWlite_all
+from numpy import arange
 
 inpath="/home/bianchi/CMSSW_5_3_3_patch2/src/Bianchi/TTHStudies/root/"
-samples = [
-    #[["TTV"],     5],
-    [["SingleT"], 1],
-    #[["DiBoson"], 5],
-    #[["TTJetsBB"],20],
-    #[["TTJetsBJ"],20],
-    #[["TTJetsJJ"],20],
-    #[["TTH125"],   5],
-    #[["EWK"],     10],
-    #[["Run2012_SingleMu", "Run2012_SingleElectron"],10 ],
-    #[["Run2012_SingleMu", "Run2012_DoubleElectron"],10 ]
+
+samples_mc = [
+    [["TTV"],      5],
+    [["SingleT"],  1],
+    [["DiBoson"],  5],
+    [["TTJetsBB"], 20],
+    [["TTJetsBJ"], 20],
+    [["TTJetsJJ"], 20],
+    [["TTH125"],   5],
+    [["EWK"],      10],
     ]
 
-binvec = cms.vdouble(0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0)
-submitDataCardMakerFWlite_all( "btag_LR", "(Vtype<=1 || Vtype==4) && numJets>=4", "test" , binvec, +1, samples, inputpath=inpath)
+samples_data_SL = [
+    [["Run2012_SingleMu"],10 ],
+    [["Run2012_SingleElectron"],10 ]
+    ]
 
+samples_data_DL = [
+    [["Run2012_SingleMu"],10 ],
+    [["Run2012_DoubleElectron"],10 ]
+    ]
+
+cuts_SL = {
+    "SL_5j3t":   "(Vtype==2 || Vtype==3) && numJets==5 && numBTagM==3",
+    "SL_g6j2t":  "(Vtype==2 || Vtype==3) && numJets>=6 && numBTagM==2",
+    "SL_g6jg4t": "(Vtype==2 || Vtype==3) && numJets>=6 && numBTagM>=4",
+    "SL_g6j3t":  "(Vtype==2 || Vtype==3) && numJets>=6 && numBTagM==3",
+    "SL_5jg4t":  "(Vtype==2 || Vtype==3) && numJets==5 && numBTagM>=4",
+    "SL_4j4t":   "(Vtype==2 || Vtype==3) && numJets==4 && numBTagM==4",
+    "SL_4j3t":   "(Vtype==2 || Vtype==3) && numJets==4 && numBTagM==3",
+#    "SL_6j": "(Vtype==2 || Vtype==3) && numJets>=6",
+#    "SL_5j": "(Vtype==2 || Vtype==3) && numJets==5",
+}
+
+#analysis = 0 SL
+#analysis = 1 DL 
+
+cuts_DL = {
+#    "DL_4j": "(Vtype==0 || Vtype==1 || Vtype==4) && numJets>=4",
+#    "DL_4j2t": "(Vtype==0 || Vtype==1 || Vtype==4) && numJets>=4 && numBTagM >=2",
+#    "DL_4j4t": "(Vtype==0 || Vtype==1 || Vtype==4) && numJets>=4 && numBTagM >=4",
+    }
+
+
+variables = {
+    "btag_LR": [0, 1, 10], # (minbin, maxbin, nbins)
+#    "jetsAboveCut": [0, 10, 10],
+#    "numBTagM": [0, 7, 7], #Fixme -- need to change to allow int values
+    }
+
+
+for var in variables:
+    minbin = variables[var][0]
+    maxbin = variables[var][1]
+    nbins = variables[var][2]
+    step = maxbin*1.0/nbins
+
+    binvec = cms.vdouble( arange(minbin, maxbin + step, step) )
+    print "binvec = " + str(binvec)
+
+    print "Submitting SL jobs... "
+    for cut in cuts_SL:
+        print "Submit jobs for var: " + var + ", cut = " + cut
+        print "-----------------------------------------------"
+        submitDataCardMakerFWlite_all( "mc_" + var + "_" + cut, cuts_SL[cut], var + "_" + cut , binvec, 0, samples_mc, inputpath=inpath)
+        submitDataCardMakerFWlite_all( "data_" + var + "_" + cut, cuts_SL[cut], var + "_" + cut , binvec, 0, samples_data_SL, inputpath=inpath)
+
+    print "Submitting DL jobs... "
+    for cut in cuts_DL:
+        print "Submit jobs for var: " + var + ", cut = " + cut
+        print "-----------------------------------------------"
+        submitDataCardMakerFWlite_all( "mc_" + var + "_" + cut , cuts_DL[cut], var + "_" + cut , binvec, 1, samples_mc, inputpath=inpath)
+        submitDataCardMakerFWlite_all( "data_" + var + "_" + cut, cuts_DL[cut], var + "_" + cut , binvec, 1, samples_data_DL, inputpath=inpath)
+    print " ...done"
--- root/datacards/hadd_all.sh
+++ root/datacards/hadd_all.sh
@@ -28,21 +28,31 @@ PROCESSES=(
 "Run2012_SingleMu"
 )
 
+CUTS=(
+"SL_g6j2t"
+"SL_g6jg4t"
+"SL_4j4t"
+"DL_4j2t"
+"DL_4j4t"
+)
+
 for PROC in ${PROCESSES[@]} 
 do
-  echo "Start processing: "$PROC
-  if ls ${INDIR}/*_${PROC}_*.root &> /dev/null ; then # check whether input files exist
-      INFILENAMES=(`ls ${INDIR}/*_${PROC}_*.root`)
-  else
-      echo $PROC input files not found
-      continue
-  fi
+  for CUT in ${CUTS[@]}
+  do
+    echo "Start processing: "$PROC
+    if ls ${INDIR}/*${CUT}*_${PROC}_*.root &> /dev/null ; then # check whether input files exist
+	INFILENAMES=(`ls ${INDIR}/*${CUT}*_${PROC}_*.root`)
+    else
+	echo $PROC input files not found
+	continue
+    fi
 
-  INFILENAME=$(basename `echo ${INFILENAMES[0]} | cut -d'.' --complement -f2-`) #drop .root extension and path
-  INFILENAME=${INFILENAME%${PROC}*}"$PROC" # drop _xx after process name
-  echo Merging to output file: $INFILENAME".root"
+    INFILENAME=$(basename `echo ${INFILENAMES[0]} | cut -d'.' --complement -f2-`) #drop .root extension and path
+    INFILENAME=${INFILENAME%${PROC}*}"$PROC" # drop _xx after process name
+    echo Merging to output file: $INFILENAME".root"
 
-  hadd -f ${OUTDIR}/${INFILENAME}".root" ${INDIR}/${INFILENAME}*.root
-
+    hadd -f ${OUTDIR}/${INFILENAME}".root" ${INDIR}/${INFILENAME}*.root
+  done
 done
 
