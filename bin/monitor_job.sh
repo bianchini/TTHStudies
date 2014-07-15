dbc0c9c87056fcf7a7bfbffde13d17650b6be437
Lorenzo Bianchini <lore.bianchini@gmail.com>
2014-07-15 17:12:18 +0300

    monitor

--- bin/monitor_job.sh
+++ bin/monitor_job.sh
@@ -7,6 +7,8 @@ files=$(ls slurm-*)
 for file in ${files}
 do
   is=`tail -n 50 $file | grep "Job done in"`
+  #is=`tail -n 50 $file | grep "Missing histogram for a process. Return."`
+  #is=`tail -n 50 $file | grep "Recap"`
   if [ -z "$is" ]; then
       echo $file
       echo `cat $file | grep "cannot remove"` >> log.txt
