6f0e1f8d1c0f99856a9e7b916b638fafbeda9b4c
Lorenzo Bianchini <lore.bianchini@gmail.com>
2014-08-27 17:21:30 +0300

    analyze

--- root/AnalyzeHepMC_ttbb.C
+++ root/AnalyzeHepMC_ttbb.C
@@ -12,6 +12,7 @@
 #include "TMatrixTBase.h"
 #include "TFile.h"
 #include "TTree.h"
+#include "TTreeFormula.h"
 #include "TString.h"
 #include "TSystem.h"
 #include "TROOT.h"
@@ -40,7 +41,6 @@
 #include "TLorentzVector.h"
 
 
-#define NJETSMAX 99
 #define SAVE 1
 
 typedef struct 
@@ -110,10 +110,15 @@ typedef struct
 
 
 float dR( TLorentzVector reco, TLorentzVector gen){
-  return TMath::Sqrt( (reco.Eta()-gen.Eta())*(reco.Eta()-gen.Eta()) +  TMath::ACos( TMath::Cos(reco.Phi()-gen.Phi()) )*TMath::ACos( TMath::Cos(reco.Phi()-gen.Phi()) ) )  ;
+  return TMath::Sqrt( (reco.Eta()-gen.Eta())*(reco.Eta()-gen.Eta()) +  
+		      TMath::ACos( TMath::Cos(reco.Phi()-gen.Phi()) )*
+		      TMath::ACos( TMath::Cos(reco.Phi()-gen.Phi()) ) )  ;
 }
 
-void analyzeHepMC( TString sample = "test", float fact = 1000 ){ 
+
+using namespace std;
+
+void analyzeHepMC( TString sample = "test", TCut cut = "type==6", float fact = 0.01 ){ 
 
   gStyle->SetOptStat(0);
   gStyle->SetTitleFillColor(0);
@@ -144,16 +149,18 @@ void analyzeHepMC( TString sample = "test", float fact = 1000 ){
   leg->SetFillColor(10);
   leg->SetTextSize(0.04); 
 
-  TFile* f = TFile::Open( "/hdfs/local/bianchi/Sherpa/test/ROOT/ME_hepMC_"+sample+".root" );
+  TFile* f = TFile::Open( "./files/Sherpa/MEM/hepMC_Sherpa_gen_std_"+sample+".root" );
   if(f==0) return;
   if(f->IsZombie()) return;
   
-  TH1D* h  = new TH1D( "h_"+sample,  sample+" ; w; d#sigma/dw", 20, 0, 1);
+  TH1D* h  = new TH1D( "h_"+sample,  sample+" ; w; d#sigma/dw", 12, 0, 1);
   h->Sumw2();
+  h->SetLineWidth(2);
 
   TTree* t = (TTree*)f->Get("tree");
   if(t==0) return;
 
+  TTreeFormula* treeformula = new TTreeFormula("cat_selection", cut , t );
 
   float massH = 125. ;
 
@@ -191,8 +198,49 @@ void analyzeHepMC( TString sample = "test", float fact = 1000 ){
   t->SetBranchAddress("SCALEsyst",    SCALEsyst   );      
 
   Long64_t nentries = t->GetEntries();
+  int num_of_trial  = 0;
+
+  Long64_t quantile20 = nentries*1/5;
+  Long64_t quantile40 = nentries*2/5;
+  Long64_t quantile60 = nentries*3/5;
+  Long64_t quantile80 = nentries*4/5;
 
   for (Long64_t i = 0; i < nentries ; i++){
+
+    if( i==0 ){
+      cout << "\e[1;32m   [                         ] (0%)\r\e[0m";
+      cout.flush();
+    }
+    if( i==quantile20 ){
+      cout << "\e[1;32m   [#####                    ] (20%)\r\e[0m";
+      cout.flush();
+    }
+    if( i==quantile40 ){
+      cout << "\e[1;32m   [##########               ] (40%)\r\e[0m";
+      cout.flush();
+    }
+    if( i==quantile60 ){
+      cout << "\e[1;32m   [###############          ] (60%)\r\e[0m";
+      cout.flush();
+    }
+    if( i==quantile80 ){
+      cout << "\e[1;32m   [####################     ] (80%)\r\e[0m";
+      cout.flush();
+    }
+    if( i==(nentries-1) ){
+      cout << "\e[1;32m   [#########################] (100%)\r\e[0m";
+      cout.flush();
+      cout << endl;
+    }
+
+    t->LoadTree(i);
+    if( treeformula-> EvalInstance() == 0){
+      //t->GetEntry(i);
+      //num_of_trial += SCALEsyst[1];
+      continue;
+    }
+
+    num_of_trial += SCALEsyst[1];
 
     // first of all, get the entry
     t->GetEntry(i);
@@ -212,7 +260,7 @@ void analyzeHepMC( TString sample = "test", float fact = 1000 ){
       for( int perm_it = 0 ; perm_it<nPermut_s ; perm_it++){		
 	float ME_prob     = p_vsMH_s[mH_it*nPermut_s + perm_it];
 	float bb_prob     = p_tt_bb[perm_it] ;
-	p_125_all_s      += bb_prob;//ME_prob;
+	p_125_all_s      += ME_prob;
       }
     }
     
@@ -228,7 +276,7 @@ void analyzeHepMC( TString sample = "test", float fact = 1000 ){
 	float ME_prob     = p_vsMT_b[mT_it*nPermut_b + perm_it];
 	float bb_prob     = p_tt_bb[perm_it] ;
 	float jj_prob     = p_tt_jj[perm_it] ;
-	p_125_all_b      += jj_prob;//ME_prob;
+	p_125_all_b      += ME_prob;
       }
     }
     
@@ -243,12 +291,133 @@ void analyzeHepMC( TString sample = "test", float fact = 1000 ){
     
   }
  
+  cout << "Total entries = "       << h->GetEntries() << endl;
+  cout << "Total num of trials = " << num_of_trial << endl;
+  h->Scale(1./num_of_trial/h->GetBinWidth(1));
+  cout << "Total x-section = "       << h->Integral("width") << endl;
+
   h->Draw("HISTE");
 
-  fout->cd();    
-  h->Write("", TObject::kOverwrite);
-  fout->Close();
+  if(SAVE && fout){
+    fout->cd();    
+    h->Write("", TObject::kOverwrite);
+    fout->Close();
+    f->Close();
+  }
 
-  //f->Close();
+  return;
  
+}
+
+
+
+void analyzeHepMC_all(){
+
+  vector<TString> samples;
+  samples.push_back("default");
+  samples.push_back("defaultX05");
+  //samples.push_back("defaultX2");
+  samples.push_back("glo_soft");
+  samples.push_back("R_Mbb");
+  samples.push_back("Q_CMMPS");
+  samples.push_back("NNPDF");
+  samples.push_back("MSTW");
+  samples.push_back("CSS_KIN");
+  samples.push_back("MPI_up");
+  samples.push_back("MPI_down");
+
+
+  for(unsigned int s = 0 ; s < samples.size() ; s++ ){
+    analyzeHepMC( samples[s], "type==6", 0.015);
+  }
+
+}
+
+
+void make_comparison(){
+
+  gStyle->SetOptStat(0);
+  gStyle->SetTitleFillColor(0);
+  gStyle->SetCanvasBorderMode(0);
+  gStyle->SetCanvasColor(0);
+  gStyle->SetPadBorderMode(0);
+  gStyle->SetPadColor(0);
+  gStyle->SetTitleFillColor(0);
+  gStyle->SetTitleBorderSize(0);
+  gStyle->SetTitleH(0.07);
+  gStyle->SetTitleFontSize(0.1);
+  gStyle->SetTitleStyle(0);
+  gStyle->SetTitleOffset(1.3,"y");
+
+  TFile* f = TFile::Open("plots_HepMC.root");
+
+  vector<TString> samples;
+  samples.push_back("default");
+  samples.push_back("defaultX05");
+  //samples.push_back("defaultX2");
+  samples.push_back("glo_soft");
+  samples.push_back("R_Mbb");
+  samples.push_back("Q_CMMPS");
+  samples.push_back("NNPDF");
+  samples.push_back("MSTW");
+  samples.push_back("CSS_KIN");
+  samples.push_back("MPI_up");
+  samples.push_back("MPI_down");
+
+
+  TCanvas *c1 = new TCanvas("c1","",5,30,650,1000);
+  c1->SetGrid(0,1);
+  c1->SetFillStyle(4000);
+  c1->SetFillColor(10);
+  c1->SetTicky();
+  c1->SetObjectStat(0);
+
+  float histsize   = 0.5;
+  float histmargin = 0.07;
+  int variations = int(samples.size())-1;
+  float step     = (histsize-histmargin)/variations;
+
+  map<TString, TPad*> amap;
+
+  for( unsigned int s = 0 ; s < samples.size() ; s++ ){
+
+    TPad *p0 = 0;
+    if(s==0) 
+      p0 = new TPad("p_"+samples[s],"",0, histsize ,1,1);
+    else
+      p0 = new TPad("p_"+samples[s],"",0, step*s   ,1, step*(s+1));
+
+    p0->SetGrid(0,0);
+    p0->SetFillStyle(4000);
+    p0->SetFillColor(10);
+    p0->SetTicky();
+    p0->SetObjectStat(0);
+    p0->Draw(); 
+    amap.insert( make_pair(samples[s], p0) );
+  }
+
+ 
+  
+  for( unsigned int s = 0 ; s < samples.size() ; s++ ){
+    TH1F* h = (TH1F*)f->Get("h_"+samples[0]);
+    (amap.find(samples[s])->second)->cd();
+    TH1F* h0 = (TH1F*)f->Get("h_default");
+
+    h->Divide(h, h0, 1., 1.);
+    h->GetYaxis()->SetRangeUser(-1,1);
+    h->GetYaxis()->SetTitle( samples[s] );
+
+    h->Draw("HISTE");
+  }
+
+
+  /*
+  TLegend* leg = new TLegend(0.52,0.60,0.77,0.88,NULL,"brNDC");
+  leg->SetFillStyle(0);
+  leg->SetBorderSize(0);
+  leg->SetFillColor(10);
+  leg->SetTextSize(0.04); 
+  */
+  
+
 }
